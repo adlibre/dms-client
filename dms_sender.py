df5fa1a3d282c6e547a196637d797bc165a4e425
garmoncheg <garmon1@gmail.com>
2012-08-08 10:40:55 +0300

    Help Fix

--- dms_sender.py
+++ dms_sender.py
@@ -64,7 +64,7 @@ Uploads file/directory into Adlibre DMS Api, depending on options/config specifi
 Looks for options file called options.conf in the folder directory and uses it's data for posting.
 
 In order to function it must have configuration file, usually called '""" + DEFAULT_CFG_FILE + """'.
-you may override those settings by calcifying alternative configuration file with '-config' key.
+You may override those settings by specifying alternative configuration file with '-config' key paramether.
 
 Available options:
 (Config file options are marked in [] and are equivalent)
