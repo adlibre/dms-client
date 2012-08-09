7136e72e1a279b37ca617ae3ffe88d5077fc72a5
garmoncheg <garmon1@gmail.com>
2012-08-09 09:08:54 +0300

    Added compile method notice

--- compile_w32.py
+++ compile_w32.py
@@ -1,3 +1,23 @@
+"""
+Compile script for py2exe.
+
+In order to compile this you need to install py2exe.
+From here:
+http://sourceforge.net/projects/py2exe/files/py2exe/
+Compile environment must be win32 (Win XP, Win Server and so on...)
+You must follow instructions from here:
+http://www.py2exe.org/index.cgi/Tutorial
+
+In short:
+you must run:
+python compile_w32.py py2exe
+
+It will produce 2 folders /build/ nad /dist/
+
+/dist/ is your distributive version.
+Rename it and use like a program distribution.
+"""
+
 from distutils.core import setup
 import py2exe
 
