2724093b6934bc7e95776c74dd2ddc5dc0e21d34
Andrew Cutler <macropin@gmail.com>
2012-11-05 16:43:07 +1100

    Initial add of setup.py.

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+from dms_client import __version__
+
+
+setup(name='dms_client',
+    version=__version__,
+    long_description=open('README.md').read(),
+    url='https://github.com/adlibre/dms-client',
+    packages=[],
+    scripts=['dms_client.py'],
+    package_data={
+            'dms_client': [
+		        'LICENSE',
+                'README.md',
+                '__init__.py',
+                'dms_client.py',
+            ],
+        },
+    data_files=[
+        ('dms_client', ['dms_client.cfg'])
+        ],
+    install_requires=[
+            # Python :)
+        ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        ],
+)
+
+
